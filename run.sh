c466c36f222e304d1f08529226a3cfb263ba1426
Troy Black <troydblack@gmail.com>
2019-02-24 18:10:40 -0600

    Removing sudo run requirements

--- run.sh
+++ run.sh
@@ -8,5 +8,8 @@ source venv/bin/activate
 echo "Updating G-Frame repo"
 git pull --rebase --autostash
 
+echo "Checking for updates to requirements.txt"
+pip install -r requirements.txt
+
 echo "Starting Flask..."
-export FLASK_APP=gframe.app && python -m flask run
+export FLASK_APP=gframe.app && python -m flask run &
